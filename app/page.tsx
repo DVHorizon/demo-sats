13cc618df8fe262a4f774bf4d684a3fc21694e9f
Dvhorizon <dangvanha123b@gmail.com>
2026-08-03 11:50:28 +0700

    feat: Hoàn thành phần sats in the news

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/component/home/Hero";
+import SatsInTheNews from "@/component/home/SatsInTheNews";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <SatsInTheNews />
     </main>
   );
 }
